be0d1c59049f271afd034412bf372ee04ad0f42b
Mamadu Bah <mamadub03>
2024-11-16 16:11:17 -0500

    completed the first index url

--- tryhard_project/tryhard_project/urls.py
+++ tryhard_project/tryhard_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
+from tryhard_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^$',views.index,name='index')
 ]
