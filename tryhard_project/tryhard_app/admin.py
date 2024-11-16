99152c98c60919694f6b44384474609bdcfd60da
Mamadu Bah <mamadub03>
2024-11-16 17:37:23 -0500

    completed the user account model

--- tryhard_project/tryhard_app/admin.py
+++ tryhard_project/tryhard_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+    list_display = ['username', 'name', 'total_time']  # Add total_time to list_display
+    search_fields = ['username', 'name']
+
+    # Specify fields for the form (this includes 'total_time')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('name', 'total_time')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- tryhard_project/tryhard_project/settings.py
+++ tryhard_project/tryhard_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tryhard_app',
 ]
 
 MIDDLEWARE = [
