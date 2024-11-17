e364f9bd485d05cf46486bcb550c0b274b8e7b6f
Mamadu Bah <mamadub03>
2024-11-17 01:47:45 -0500

    created user account login session

--- tryhard_project/tryhard_app/views.py
+++ tryhard_project/tryhard_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth import authenticate, login as auth_login
 from .models import CustomUser
 # Create your views here.
 
@@ -10,7 +12,35 @@ def leaderboard(request):
     return render(request,'leaderboard.html')
 
 def login(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        next_url = request.POST.get('next', '/timer/')  # Default to '/dashboard/'
+
+        # Fetch the user from the database
+        try:
+            user = CustomUser.objects.get(username=username)
+        except CustomUser.DoesNotExist:
+            print('not found')
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+        
+        # Check if the password is correct using check_password
+        if check_password(password,user.password):  # Compare plain password with hashed password
+            # Set session variables to indicate the user is logged in
+            # request.session['user_id'] = user.id
+            request.session['username'] = user.username
+            return render(request, 'timer.html',{'username':request.session.get('username')})
+  # Redirect to the dashboard or next URL
+        else:
+            
+            
+            print(password)
+            print(user.password)
+
+            # Return an error message if the password is incorrect
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+
+    return render(request, 'login.html',{'username':request.session.get('username')})
 
 def signup(request):
     if request.method == 'POST':
@@ -24,7 +54,7 @@ def signup(request):
 
         # Create and save the new user with a hashed password
         user = CustomUser(name=name, username=username)
-        user.password = make_password(password1)  # Hash the password before saving
+        user.password = password1  # Hash the password before saving
         user.save()
 
         return redirect('login')  # Redirect to login page after sign-up
